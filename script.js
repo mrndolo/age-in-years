bdd927384cf4ce99ff336595b8dd2497d0041196
mrndolo <callistomutunga@gmail.com>
2024-08-01 14:51:33 +0300

    Added logic to compute age

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function calculateAge() {
       resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
     }
   }
+
+  function getAge(birthdayValue) {
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthdayValue);
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+  
+    if (
+      month < 0 ||
+      (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+    ) {
+      age--;
+    }
+  
+    return age;
+  }
+  
+  btnEl.addEventListener("click", calculateAge);
